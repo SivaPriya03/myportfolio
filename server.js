e03b8eb51f4d862ce8b6896f0a3bf68de7b2e6a6
SivaPriya03 <sivapriya1700@gmail.com>
2021-11-27 22:41:55 +0530

    Port changes

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
+const port = process.env.PORT || 3001;
 function startServer() {
   const app = express();
   const configureLoaders = require('./loaders');
   configureLoaders(app);
 
-  app.listen(3000, () => {
+  app.listen(port, () => {
     console.log('Your app is listening on port 3000');
   });
 }
