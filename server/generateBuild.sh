9a8f11588f85ffa58a52527325677e3cc61cf262
SivaPriya03 <sivapriya1700@gmail.com>
2021-11-27 19:33:42 +0530

    cache clean

--- server/generateBuild.sh
+++ server/generateBuild.sh
@@ -1,6 +1,13 @@
 cd ../client
+rm -r ./dist 
+rm -r ./node_modules 
+rm ./package-lock.json
+npm cache clean -f
 npm i
 npm run build
 cd ../server
+rm -r ./node_modules 
+rm package-lock.json
+npm cache clean -f
 npm i
 node server.js
